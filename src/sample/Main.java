052b1fe9023d5ea10654dae2b7dcd5c8e3b2aedf
HakunaMacouta <thomas.blanc.2@etu.u-bordeaux.fr>
2017-02-12 18:25:41 +0100

    Modif Main.java, fail de merge

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,47 +5,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-<<<<<<< HEAD
 import sample.Model.Sommet;
 
-=======
 import sample.Model.Arete;
 import sample.Model.Sommet;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 
->>>>>>> 659a1f51e926b9696f1b39347754ef2a26911c8d
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-<<<<<<< HEAD
         Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
-=======
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
->>>>>>> 659a1f51e926b9696f1b39347754ef2a26911c8d
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
-<<<<<<< HEAD
     }
 
     public static void addSommet(float x, float y) {
-        Sommet s = new Sommet(true, x,y);
-=======
+        Sommet s = new Sommet("", x,y);
 
-    }
-
-    public static void addSommet(float x, float y) {
-        Sommet s = new Sommet("name", x,y);
->>>>>>> 659a1f51e926b9696f1b39347754ef2a26911c8d
     }
 }

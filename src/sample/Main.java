083ce3ac4b3dc434a46347d617b3dceb7479dbc9
HakunaMacouta <thomas.blanc.2@etu.u-bordeaux.fr>
2017-02-12 18:30:57 +0100

    changement nom fenêtre.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Gestionnaire de graphe");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }

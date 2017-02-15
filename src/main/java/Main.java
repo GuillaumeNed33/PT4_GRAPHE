9f3ea806b7d29e6ed6e89f970d8f0eed92521a09
HakunaMacouta <thomas.blanc.2@etu.u-bordeaux.fr>
2017-02-15 15:51:49 +0100

    déplacement des fxml dans resources/ et fix des bugs de nullPointerExeption.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,7 +9,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("src/main/FXML/MainApplication.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainApplication.fxml"));
         primaryStage.setTitle("Gestionnaire de graphe");
         primaryStage.setMaximized(true);
         primaryStage.setScene(new Scene(root));
@@ -16,6 +17,5 @@ public class Main extends Application {
     }
     public static void main(String[] args) {
         launch(args);
-        /*Graphe g = new Graphe("ressources/graphvis.dot");*/
     }
 }

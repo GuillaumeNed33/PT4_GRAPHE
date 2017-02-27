cba7f7680bbc8291261017b6053ce08c0a5e1b84
mleothaud <mathieu.leothaud@etu.u-bordeaux.fr>
2017-02-27 10:53:48 +0100

    Tests graphe

--- src/test/java/GrapheTest.java
+++ src/test/java/GrapheTest.java
@@ -1,8 +1,8 @@
+import Model.Graphe;
+import Model.Sommet;
 import com.sun.glass.ui.Size;
 import org.junit.Assert;
 import org.junit.Test;
-import Model.Graphe;
-import Model.Sommet;
 
 /**
  * Created by Mathieu on 06/02/2017.
@@ -92,5 +92,47 @@ public class GrapheTest {
         Assert.assertEquals(1,g.getM_aretes().size());
         g.supprimerSommet(s);
         Assert.assertEquals(0,g.getM_aretes().size());
+    }
+
+    @Test
+    public void testIndiceDegré() {
+        Size m = new Size(10,10);
+        Graphe g = new Graphe();
+
+        Sommet s = new Sommet("s1",1,1);
+        Sommet s2 = new Sommet("s2",2,2);
+        Sommet s3 = new Sommet("s3",3,3);
+        Sommet s4 = new Sommet("s4",4,4);
+
+        g.ajouterSommet(s, m);
+        g.ajouterSommet(s2, m);
+        g.ajouterSommet(s3, m);
+        g.ajouterSommet(s4, m);
+
+        g.ajouterArete(s,s2);
+        g.ajouterArete(s,s3);
+        g.ajouterArete(s,s4);
+
+        g.setIndiceDegre(s);
+        g.setIndiceDegre(s2);
+        g.setIndiceDegre(s3);
+        g.setIndiceDegre(s4);
+        Assert.assertEquals(s.getIndice(),3);
+        Assert.assertEquals(s2.getIndice(),1);
+        Assert.assertEquals(s3.getIndice(),1);
+        Assert.assertEquals(s4.getIndice(),1);
+
+        g.ajouterArete(s2,s3);
+        g.setIndiceDegre(s2);
+        g.setIndiceDegre(s3);
+        Assert.assertEquals(s2.getIndice(),2);
+        Assert.assertEquals(s3.getIndice(),2);
+
+        g.supprimerSommet(s3);
+        g.setIndiceDegre(s);
+        g.setIndiceDegre(s2);
+        g.setIndiceDegre(s4);
+        Assert.assertEquals(s2.getIndice(),1);
+        Assert.assertEquals(s.getIndice(),2);
     }
 }

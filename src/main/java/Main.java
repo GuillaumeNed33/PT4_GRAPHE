7c42004773fb7f76ed7d02828edbcc95f5138d2c
Lentilhac <audrey.lentilhac@etu.u-bordeaux.fr>
2017-03-03 07:46:11 +0100

    initialisation du graphe avec des positions aléatoires pour avoir un affichage dès l'importation effectuée

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,19 +16,6 @@ public class Main extends Application {
         primaryStage.show();
     }
     public static void main(String[] args) {
-        /*** TEST GRAPHE ***/
-/*        Graphe g = new Graphe("ressources/sample.graphml");
-        String m_name = g.m_name;
-        Size m_size=g.m_size;
-        ArrayList<Sommet> m_sommets=g.m_sommets;
-        ArrayList<Arete> m_aretes=g.m_aretes;
-        HashMap<Sommet, ArrayList<Arete> > m_incidente=g.m_incidentes;
-        HashMap<Arete, Pair<Sommet,Sommet>> m_extremites=g.m_extremites;    // conteneur liant pour chaque arete sa paire de sommets
-        ArrayList<KeyStyleGRAPHML> m_keyGML=g.m_keyGML;
-        AlgorithmeRepresentation algoRep = g.algoRep;
-        String test = new String("rtest");
-        */
         launch(args);
-
     }
 }

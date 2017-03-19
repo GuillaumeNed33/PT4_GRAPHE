9d4b4f80f0a3d5e5436b9e3c3afebd92959d6aea
Lentilhac <audrey.lentilhac@etu.u-bordeaux.fr>
2017-03-19 10:42:00 +0100

    fix merge problems

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import Controller.FXMLController;
 import View.Graphe;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -14,14 +15,19 @@ public class Main extends Application {
         /** GESTION DES NODES **/
         View.Graphe g = new Graphe();
         VBox root = new VBox();
-        TabPane menu = FXMLLoader.load(getClass().getResource("/fxml/MainMenu.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainMenu.fxml"));
+        TabPane menu = loader.load();
         root.getChildren().addAll(menu,g.getScrollPane());
+
 
         /** PARAMETRAGE DE LA FENETRE **/
         primaryStage.setTitle("Gestionnaire de graphe");
         primaryStage.setMaximized(true);
-        primaryStage.setScene(new Scene(root));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.show();
+        FXMLController controller = (FXMLController)loader.getController();
+        controller.setVbox(root);
     }
     public static void main(String[] args) {
         launch(args);
